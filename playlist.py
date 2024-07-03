abcbf662ceaf24f30564a4f8223da214dcb253dc
nicolaics <nicolai.christian.s@gmail.com>
2024-07-03 18:52:27 +0900

    modify for queue playlist

--- playlist.py
+++ playlist.py
@@ -65,21 +65,38 @@ def sort_playlist(p_name: str, sort_params: str, sort_order: str) -> list[Song]:
 def view_all_playlists() -> list[str]:
     f_path = './playlists'
     
-    playlists_list = os.listdir(f_path)
+    playlists_list_rough = os.listdir(f_path)
 
     header_len = {
         "no": 7,
         "p_name": 32,
     }
 
+    print()
+    print("+" + ("-" * header_len['no']), end='')
+    print("+" + ("-" * header_len['p_name']), end='')
+    print("+")
+    print("| {0:5s} | {1:30s} |".format("No.", "Playlist Name"))
     print("+" + ("-" * header_len['no']), end='')
     print("+" + ("-" * header_len['p_name']), end='')
     print("+")
 
-    count = 1
+    playlists_list = []
+
+    for i in playlists_list_rough:
+        if (i.endswith(".md")) or (i == "QUEUE.bin"):
+            continue
+            
+        playlists_list.append(i)
+
+    count = 1 
 
     for i in playlists_list:
-        print(f"| {count:5s} | {i:30s} |")
+        c = str(count) + "."
+        
+        i = i.split(".")[0]
+
+        print(f"| {c:5s} | {i:30s} |")
         count += 1
 
     print("+" + ("-" * header_len['no']), end='')
@@ -89,9 +106,13 @@ def view_all_playlists() -> list[str]:
     return playlists_list   
 
 def view_songs_in_a_playlist(p_name: str):
-    p_fname = FOLDER_PATH + p_name + ".bin"
+    p_fname = FOLDER_PATH + p_name
 
-    fh = open(p_fname, 'rb')
+    try:
+        fh = open(p_fname, 'rb')
+    except:
+        print("Playlist do not exist!")
+        return
     
     songs_list = []
         
@@ -104,9 +125,9 @@ def view_songs_in_a_playlist(p_name: str):
     fh.close()
 
     print()
-    print(f"Playlist: {p_name}")
+    print(f"Playlist: {p_name.split('.')[0]}")
 
-    if p_name == 'QUEUE':
+    if p_name == 'QUEUE.bin':
         temp = []
         for i in songs_list[0]:
             temp.append(i.song)
