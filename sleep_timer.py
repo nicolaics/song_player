59b17f58c5220012e1aa4dc586a8d0b457b664f3
nicolaics <nicolai.christian.s@gmail.com>
2024-07-03 13:36:12 +0900

    implement sleep timer

--- sleep_timer.py
+++ sleep_timer.py
@@ -5,9 +5,19 @@ import player
 
 def close_app(song_q: list):
     player.save_queue(song_q)
-
     os._exit(0)
 
-def set_timer(t: float, song_q: list):
-    timer = threading.Timer(3.0, function=close_app, args=(song_q,))
+def set_timer(t: str, song_q: list):
+    parse = t.split(":")
+    hrs = float(parse[0])
+    mins = float(parse[1])
+    secs = float(parse[2])
+
+    t = secs
+    t += (mins * 60)
+    t += (hrs * 3600)
+
+    timer = threading.Timer(t, function=close_app, args=(song_q,))
     timer.start()
+
+    print(f"Timer has been set for: {hrs}h {mins}m {secs}s!")
